856868d59ad673bb3f3884c506ee30cefbd78a7a
zqy <850074816@qq.com>
2026-03-31 02:02:57 +0800

    change ip thread num

--- main.c
+++ main.c
@@ -684,7 +684,7 @@ int main(int argc, char *const *argv)
 	// 			assert(!rte_eth_dev_rx_intr_ctl_q(0 /* port id */, 0 /* queue */, RTE_EPOLL_PER_THREAD, RTE_INTR_EVENT_ADD, NULL));
 	// 	}
 	// }
-	int ip_thread_num = 4;
+	int ip_thread_num = 2;
 	int tcp_thread_num = 4;
 
 	nic_init(ip_thread_num, tcp_thread_num, max_epoll_wait_timeout_ms);
